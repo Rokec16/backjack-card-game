6b6407436eb43870c0570a921e644191e4c16ff9
rokf <rok.ficko@elrad-int.si>
2023-10-10 13:56:58 +0200

    CHANGED: - encapsulated much of the core game functionality - tidied up the documentation

--- main.py
+++ main.py
@@ -2,97 +2,155 @@ from cards import Deck
 from players import Player
 
 
-def hit(player: Player, deck: Deck):
-    """Add a card to players hand and adjust for aces."""
+def hit(player: Player, deck: Deck) -> None:
+    """Add a card to the selected players hand and adjust for aces."""
 
     player.hand.add_card(deck.deal_one_card())
     player.hand.adjust_for_ace()
 
 
-def show_players_cards(player: Player, dealer: Player):
+def show_players_cards(player: Player, hide_one: bool = False) -> None:
+    """Displays playing cards currently held in the selected players hand.
 
-    print('Players cards are: ', *player.hand.cards, sep='\n')
+    Parameters:
+    -----------
+        player : Player
+            Instance of the Player which cards to display.
+        hide_one : bool, optional
+            Determines if first card of the hand is hidden. Defaults to False.
+    """
+
+    if hide_one:
+        print(f'{player.name}s cards are: ')
+        print('Hidden card.')
+        for card in player.hand.cards[1:]:
+            print(card)
+    else:
+        print(f'{player.name}s cards are: ', *player.hand.cards, sep='\n')
 
     print('--------------------')
 
-    print('Dealers cards are:')
-    print('Hidden card.')
-    for card in dealer.hand.cards[1:]:
-        print(card)
 
-    print()
+def setup_the_game(player_name: str,
+                   player_chips: int) -> (Deck, Player, Player):
+    """Creates a shuffled deck of cards, setups two players holding two cards.
+
+    Parameters:
+    ----------
+    player_name : str
+        Name of the player.
+    player_chips: int
+        Amount of chips that the players holds at the start of the game.
+
+    Returns:
+    --------
+        tuple: Holding objects of deck, player, dealer.
+    """
+
+    deck = Deck()
+    deck.shuffle_cards()
+
+    player = Player(player_name, player_chips)
+    dealer = Player('Dealer')
+
+    player.hand.add_card(deck.deal_one_card())
+    dealer.hand.add_card(deck.deal_one_card())
+
+    player.hand.add_card(deck.deal_one_card())
+    dealer.hand.add_card(deck.deal_one_card())
+
+    return deck, player, dealer
 
 
-def show_all_cards(player: Player, dealer: Player):
-    print('Players cards are:')
-    for card in player.hand.cards:
-        print(card)
+def play_the_game(deck: Deck, player: Player, dealer: Player) -> None:
+    """Adds cards to players and dealers hands according to rules of Blackjack.
 
-    print('--------------------')
+    First comes the players turn. Player decides to hit or stand and receives
+    a new card if he chooses hit. Players turn ends if he decides to stand
+    or value of his cards surpasses 21.
+    Dealer hits until his score surpasses 17.
 
-    print('Dealers cards are:')
-    for card in dealer.hand.cards:
-        print(card)
+    Parameters:
+    -----------
+        deck : Deck
+            Instance of the deck of cards.
+        player : Player
+            Instance of the Player class representing the player.
+        dealer : Player
+            Instance of the Player class representing the dealer.
+    """
 
-    print(f'Value of players cards is: {player.hand.value}')
-    print(f'Value of dealers cards is: {dealer.hand.value}')
+    # Players turn.
+    while True:
 
-    print()
+        if player.hit_or_stand():
+            hit(player, deck)
+            show_players_cards(player)
+        else:
+            print(f'Player {player.name} decided to stand.')
+            break
+
+        # If players hand value reaches 21, he has lost.
+        if player.hand.value > 21:
+            break
+
+    # Dealers turn
+    if player.hand.value > 21:
+        # There is no need to play dealers part if player has already lost.
+        pass
+    else:
+        while dealer.hand.value < 17:
+            hit(dealer, deck)
 
 
-def main():
+def check_results(player: Player, dealer: Player):
+    """Checks the victory conditions and distributes the chips accordingly.
+
+    Parameters:
+    -----------
+        player : Player
+            Instance of the Player class representing the player.
+        dealer : Player
+            Instance of the Player class representing the dealer.
+    """
+
+    if dealer.hand.value > 21:
+        print(f'Dealer busts. {player.name} wins!')
+        player.chips += player.bet * 2
+    elif player.hand.value > 21:
+        print('Player busts!')
+    elif player.hand.value == dealer.hand.value:
+        print('Tie!')
+    elif player.hand.value > dealer.hand.value:
+        print(f'{player.name} wins!')
+        player.chips += player.bet * 2
+    else:
+        print('Dealer wins!')
+
+
+def game():
 
     print('Welcome to the game of Console Blackjack. Good luck!')
 
     while True:
 
-        deck_of_cards = Deck()
-        deck_of_cards.shuffle_cards()
+        deck_of_cards, player, dealer = setup_the_game('Player 1', 100)
 
-        player = Player('Player1', 100)
-        dealer = Player('Dealer')
+        player.take_bet()
+        print(f'Players bets {player.bet} chips.')
 
-        player.hand.add_card(deck_of_cards.deal_one_card())
-        dealer.hand.add_card(deck_of_cards.deal_one_card())
+        show_players_cards(player)
+        show_players_cards(dealer, hide_one=True)
 
-        player.hand.add_card(deck_of_cards.deal_one_card())
-        dealer.hand.add_card(deck_of_cards.deal_one_card())
+        play_the_game(deck_of_cards, player, dealer)
 
-        player_bet = player.take_bet()
-        print(f'Players bets {player_bet} chips.')
-        print()
+        show_players_cards(player)
+        show_players_cards(dealer)
 
-        show_players_cards(player, dealer)
+        print(f'Value of players cards is: {player.hand.value}')
+        print(f'Value of dealers cards is: {dealer.hand.value}')
 
-        while True:
-
-            if player.hit_or_stand():
-                hit(player, deck_of_cards)
-            else:
-                print(f'Player {str(player)} decided to stand.')
-
-            if player.hand.value > 21:
-                break
-
-            if dealer.hand.value < 17:
-                hit(dealer, deck_of_cards)
-            else:
-                break
-
-            show_players_cards(player, dealer)
-
-        show_all_cards(player, dealer)
-
-        if dealer.hand.value > 21:
-            print('Dealer busts!')
-            player.chips += player_bet * 2
-        elif player.hand.value > 21:
-            print('Player busts!')
-        elif player.hand.value > dealer.hand.value:
-            print('Player wins!')
-            player.chips += player_bet * 2
-        else:
-            print('Dealer wins!')
+        check_results(player, dealer)
 
         print(f'\nPlayer holds {player.chips} chips.')
 
@@ -104,4 +162,4 @@ def main():
 
 
 if __name__ == "__main__":
-    main()
+    game()
